5df45e1493f3c8beede53a59eaa38e02a05b8040
Maximilian Georg Kurzawski <maximilian.kurzawski@gmail.com>
2024-10-19 22:55:16 +0200

    Binary Tree Postorder Traversal

--- 145BinayTreePostorderTraversal/main.go
+++ 145BinayTreePostorderTraversal/main.go
@@ -1,2 +1,18 @@
 package main
 
+type TreeNode struct {
+    Val int
+    Left *TreeNode
+    Right *TreeNode
+}
+
+func postorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return []int{}
+	}
+	result := make([]int, 0)
+	result = append(result, postorderTraversal(root.Left)...)
+	result = append(result, postorderTraversal(root.Right)...)
+	result = append(result, root.Val)
+	return result
+}
