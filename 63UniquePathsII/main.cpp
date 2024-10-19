6690ead60732ffc49f1ae2f379aff655774f45e8
Maximilian Georg Kurzawski <maximilian.kurzawski@gmail.com>
2024-10-19 19:38:00 +0200

    63 Unique Paths 2 main.cpp

---
+++ 63UniquePathsII/main.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<vector>
+
+using std::cout;
+using std::endl;
+using std::vector;
+
+class Solution 
+{
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) 
+    {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) 
+        {
+            return 0;
+        }
+        
+        // Create a DP table
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        
+        // Initialize the starting point
+        dp[0][0] = 1;
+        
+        // Fill the DP table
+        for (int i = 0; i < m; i++) 
+        {
+            for (int j = 0; j < n; j++) 
+            {
+                if (obstacleGrid[i][j] == 1) 
+                {
+                    dp[i][j] = 0;
+                } 
+                else 
+                {
+                    if (i > 0) 
+                    {
+                        dp[i][j] += dp[i-1][j];
+                    }
+                    if (j > 0) 
+                    {
+                        dp[i][j] += dp[i][j-1];
+                    }
+                }
+            }
+        }
+        
+        return dp[m-1][n-1];
+    }
+};
